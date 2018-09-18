de54b981da85b423fa2f0f51ceeb77e87a7fc76d
Dean Hooker <deanhooker91@gmail.com>
2018-09-17 21:16:27 -0600

    updated project to use whitenoise

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -153,9 +153,9 @@ if cwd == '/app' or cwd[:4] == '/tmp':
 
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # STATICFILES_DIRS = (
+    #     os.path.join(BASE_DIR, 'static'),
+    # )
 
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
